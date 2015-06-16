072d4149ae2aa9bc0add0b4856bb7aaee48419fc
AltumTecnologia <altum@altumDeveloper.(none)>
2015-06-16 11:46:22 -0300

    tst

--- index.js
+++ index.js
@@ -10,45 +10,31 @@ app.set('views', './');
 app.use(express.static('./'));
 
 function atualizaBanco (resp) {
-		var data = '';
+		var arrayRet = '';
 
  	    resp.on('data', function (chunk) {
- 	    	data += chunk;
+ 	    	arrayRet += chunk;
   		});
 
   		resp.on('end', function () {
-  			data = JSON.parse(data);
+  			arrayRet = JSON.parse(arrayRet);
+  			res.json(arrayRet);
   		})
 		
-		.on('error', function(e) {
+		resp.on('error', function(e) {
   	  		console.log("Got error: " + e.message);
 		})		
 	}
 
 
 app.get('/', function(req, res) {
-	http
-	.get("http://sistema.instakioski.com.br/bling.php", atualizaBanco) 
-	.then(function (){res.render('index.html')};
+;
+	res.render('index.html');
 
 });
 app.get('/getData', function(req, res){
-	http.get("http://sistema.instakioski.com.br/bling.php", function(resp) {
-		var data = '';
-
- 	    resp.on('data', function (chunk) {
- 	    	data += chunk;
-  		});
-
-  		resp.on('end', function () {
-  			data = JSON.parse(data);
-  			res.json(data);
-  		})
-		
-		})
-		.on('error', function(e) {
-  	  		console.log("Got error: " + e.message);
-		});
+	http.get("http://sistema.instakioski.com.br/bling.php", atualizaBanco); 
+	console.log('haha')	
 });
 
 MongoNative.connect('mongodb://localhost/envia').then(function (db){
