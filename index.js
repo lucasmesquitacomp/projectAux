ba21fad8e4cc77fe641001551fd02e3acd763048
AltumTecnologia <altum@altumDeveloper.(none)>
2015-06-26 14:09:35 -0300

    Login Pronto

--- index.js
+++ index.js
@@ -4,20 +4,162 @@ var http = require('http');
 var MongoNative = require('mongo-native');
 var bodyParser = require('body-parser');
 var Q = require('q');
+
+
+	//Auth variables
+	var passport = require('passport');
+	var LocalStrategy = require('passport-local').Strategy;
+	var mongodb = require('mongodb');
+	var mongoose = require('mongoose');
+	var bcrypt = require('bcrypt');
+	var SALT_WORK_FACTOR = 10;
+		//Auth express variables
+		var morgan = require('morgan');
+		var cookieParser = require('cookie-parser');
+		var methodOverride = require('method-override');
+		var session = require('express-session');
+	//Auth connection
+	mongoose.connect('localhost','pedidos');
+	var dbm = mongoose.connection;
+	dbm.on('error', console.error.bind(console, 'connection error:'));
+	dbm.once('open', function callback(){
+		console.log('Mongoose user Data');
+	});
+
+	//Auth Mongoose Schema
+	var userSchema = mongoose.Schema({
+		username: {type: String, require: true, unique:true},
+		email: { type: String, required: true, unique: true},
+		password: {type: String, required: true},
+	});
+
+	//Auth Encription
+	userSchema.pre('save', function(next){
+		var user = this;
+		if(!user.isModified('password')) return next();
+
+		bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+			if(err) return next(err);
+
+			bcrypt.hash(user.password, salt, function(err, hash){
+				if(err) return next(err);
+				user.password = hash;
+				next();
+			});
+		});
+	});
+
+	//Auth Connect 
+
+	userSchema.methods.comparePassword = function(candidatePassword, cb){
+		bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+			if(err) return cb (err);
+			cb(null, isMatch);
+		});
+	};
+
+	//Auth add user(temporrario = alimentar);
+
+	var User = mongoose.model('User', userSchema);
+	var user = new User({username: 'Altum', email: 'altum@gmail.com',password:'altum'});
+	user.save(function(err){
+		if(err){
+			console.log(err);
+		}else{
+			console.log('user: '+ username +'salvo');
+		};
+	});
+
+	//Auth Passport config
+	passport.serializeUser(function(user, done){
+		done(null, user.id);
+	});
+
+	passport.deserializeUser(function(id, done){
+		User.findById(id, function (err, user){
+			done(err, user);
+		});
+	});
+
+	passport.use(new LocalStrategy(function(username, password, done){
+		User.findOne({username: username},function (err, user){
+			if(err){ 
+				return done(err);
+			}
+			if(!user){
+				return done(null, false,{message: "Usuário:"+ username+" não cadastrado"});
+			}
+			user.comparePassword(password, function(err,isMatch){
+				if(err) return done(err);
+				if(isMatch){
+					return done(null, user);
+				} else{
+					return done(null, false, {message:'Invalid password'});
+				}
+			});
+		});
+	}));
+
+
+//Auth Functions
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect('/login');
+}
+//page configure
 var app = express();
+var router = express.Router();
 
+	app.use(express.static('./'))
 
-app.set('views', './');
+	app.set('views',__dirname + '/views');
+	
+	app.use(express.static(__dirname + '/../../public'));
+	app.set('view engine', 'ejs');
+	app.engine('ejs', require('ejs-locals'));
+	app.use(morgan());
+	app.use(cookieParser());
+	app.use(bodyParser());
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({extended:true}));	
+	app.use(methodOverride());
+	app.use(session({secret: 'altum tecnologia'}));
 
-app.use(express.static('./'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-
-
-app.get('/', function(req, res) {
-	res.render('index.html');
-
+//passport
+	app.use(passport.initialize());
+	app.use(passport.session());
+	
+app.get('/',ensureAuthenticated, function(req, res) {
+	res.render('index',{user: req.user});
 });
+app.get('/login',function( req, res){
+	res.render('login',{user: req.user, message: req.session.messages});
+})
+
+//auth post
+app.post('/login', function(req, res, next){
+	passport.authenticate('local', function(err, user, info){
+		if(err) {
+			return next(err);
+		}
+		if (!user){
+			req.session.messages = [info.message];
+			return res.redirect('/login');
+		}
+			req.logIn(user, function(err){
+				if (err) { return next(err); }
+				return res.redirect('/');
+			});
+	})(req, res, next);
+});
+
+app.get('/logout', function(req , res){
+	req.logout();
+	res.redirect('/');
+})
+
 app.get('/refreshData', function(req, res){
 	http.get("http://sistema.instakioski.com.br/bling.php", function (resp) {
 		var data = '';
@@ -82,6 +224,12 @@ app.post('/getFilterData',function (req,res){
 		}
 
 });
+// passar por POST os dados
+// {
+// 'username' : 'HDQWHD'
+// 'password' : 'HDQWHD'
+//}
+app.post('/auth', passport.authenticate('local'));
 app.post('/updateData', function (req,res){
 	var pedidos = db.collection('pedidos')
 	var data = req.body;
@@ -98,7 +246,7 @@ app.post('/updateData', function (req,res){
 
 MongoNative.connect('mongodb://localhost/sistemaEnvio').then(function (db){
 	global.db = db;
-	app.listen(9999);
+	app.listen(9000);
 })
 
 
